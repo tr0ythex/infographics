c940cf583c5245e7cf0d26ec1622afa1a2bb2f6f
tr0ythex <tr0ythex@gmail.com>
2017-01-07 18:12:43 +0300

    Adds simple svg circle as a foundation of future pie-chart component. Updates tests

--- src/app/pie-chart/pie-chart.component.ts
+++ src/app/pie-chart/pie-chart.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-pie-chart',
-  templateUrl: './pie-chart.component.html',
-  styleUrls: ['./pie-chart.component.sass']
+    selector: 'app-pie-chart',
+    templateUrl: './pie-chart.component.html',
+    styleUrls: ['./pie-chart.component.sass']
 })
 export class PieChartComponent implements OnInit {
+    @Input() width: number;
+    @Input() height: number;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
